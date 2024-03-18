e9e6ed24a4b698f44d671c188cfef1f1b08226c9
Gabriel Jesus <gabriel.developer2@gmail.com>
2024-03-18 19:46:51 -0300

    chore: setup knex and database

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import { app } from "./app";
+import { knex } from "./database";
+
+
+app.get('/', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP Server running!')
+})
